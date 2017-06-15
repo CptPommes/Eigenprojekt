e04626bf4105ef8e5a00c8d08de9dc4647149aba
unknown <u29795@pc9327d15.vw.hs-harz.de>
2017-06-15 16:53:09 +0200

    Menu

---
+++ assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	
+
+	void Start ()
+	{
+		
+	}
+
+    public void loadLevel(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
